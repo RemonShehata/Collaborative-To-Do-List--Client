b50a0dcbb90ffb48cf6015eddf12855a3d91a0f5
RemonShehata <RemonGerges94@gmail.com>
2020-02-03 17:13:32 +0200

    Finish login & refactor connector

--- src/clientapplication/LoginController.java
+++ src/clientapplication/LoginController.java
@@ -10,6 +10,7 @@ package clientapplication;
 // 8-16 characters password with at least one digit, at least one
 // lowercase letter at least one uppercase letter, at least one
 // special character with no whitespaces
+import help.RegixMethods;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -26,6 +27,9 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javax.json.JsonObject;
+import network.JsonUtil;
+import network.RequestHandler;
 
 public class LoginController {
 
@@ -41,9 +45,9 @@ public class LoginController {
     private Hyperlink notYouHyperLink;
 
     private boolean flagEmail = false;
-    
 
-    
+    private int id = -1;
+    ActionEvent eventSource = null;
 
     /**
      *
@@ -51,31 +55,13 @@ public class LoginController {
      */
     @FXML
     private void nextButtonPressed(ActionEvent event) {
+        eventSource = event;
+
         if (flagEmail == false) {
-        validateEmail();
+            validateEmail();
         } else {
-            if (validatePassword()) {
-//          go to home page 
-//          pass id to home
-        ((Node) event.getSource()).getScene().getWindow().hide();
-        FXMLLoader fxload = new FXMLLoader(getClass().getResource("HomeView.fxml"));
-        Parent root;
-        try {
-            root = (Parent) fxload.load();
-            Stage stage = new Stage();
-       
-            stage.setScene(new Scene(root));
-            stage.initStyle(StageStyle.UNDECORATED);
-
-            stage.show();
-        } catch (IOException ex) {
-            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+            validatePassword();
         }
-
-            }
-
-        }
-
     }
 
     /**
@@ -87,31 +73,16 @@ public class LoginController {
         Parent tableViewParent;
         try {
             tableViewParent = FXMLLoader.load(getClass().getResource("RegisterView.fxml"));
-              Scene tableViewScene = new Scene(tableViewParent);
-        
-        //This line gets the Stage information
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-        
-        window.setScene(tableViewScene);
-        window.show();
+            Scene tableViewScene = new Scene(tableViewParent);
+
+            //This line gets the Stage information
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            window.setScene(tableViewScene);
+            window.show();
         } catch (IOException ex) {
             Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
         }
-      
-//        FXMLLoader fxload = new FXMLLoader(getClass().getResource("RegisterView.fxml"));
-//        Parent root;
-//        try {
-//            root = (Parent) fxload.load();
-//            Stage stage = new Stage();
-//                 RegisterController reg = (RegisterController)fxload.getController();
-//            reg.setX(17);
-//            stage.setScene(new Scene(root));
-//            stage.initStyle(StageStyle.UNDECORATED);
-//
-//            stage.show();
-//        } catch (IOException ex) {
-//            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
-//        }
 
     }
 
@@ -139,39 +110,92 @@ public class LoginController {
 
     }
 
-    private boolean validateEmail() {
-        boolean checkEmail = false;
+    private void validateEmail() {
         if (emailText.getText().isEmpty()) {
             invalidLabel.setText("Enter email");
+        } else if (RegixMethods.isValidEmail(emailText.getText())) {
 
-        } else if (true/*replace with query in db*/){//query email;
-            emailText.setVisible(false);
-            passwordText.setVisible(true);
-            notYouHyperLink.setVisible(true);
-            
-           
-            flagEmail = true;
-            invalidLabel.setText("");
-            passwordText.setText("");
-            checkEmail = true;
+            new Thread(() -> {
+                JsonObject jsonObject = JsonUtil.createJsonEmail(emailText.getText());
+                JsonObject response = new RequestHandler().makeRequest(jsonObject);
+                System.out.println(response);
+                id = JsonUtil.convertFromJsonId(response);
+                
+                Platform.runLater(() -> {
+                    if (id != -1) {
+                        emailText.setVisible(false);
+                        passwordText.setVisible(true);
+                        notYouHyperLink.setVisible(true);
+                        
+                        flagEmail = true;
+                        invalidLabel.setText("");
+                        passwordText.setText("");
+                    } else {
+                        invalidLabel.setText("Could't find your email");
+                    }
+                });
+  
+            }).start();
+
         } else {
-            invalidLabel.setText("Could't find your email");
+            Platform.runLater(() -> {
+                invalidLabel.setText("Enter valid email");
+            });   
         }
-        return checkEmail;
+        
     }
 
-    private boolean validatePassword() {
-        boolean checkPassword = false;
+    
+    private void validatePassword() {
         if (passwordText.getText().isEmpty()) {
             invalidLabel.setText("Enter password");
 
-        } else if (true /* query on password in db*/) {
-            passwordText.setText("");
-            invalidLabel.setText("");
-            checkPassword = true;
+        } else if (RegixMethods.isValidPassword(passwordText.getText())) {
+
+            new Thread(() -> {
+                JsonObject jsonObject = JsonUtil.createJsonPassword(passwordText.getText(), id);
+                JsonObject response = new RequestHandler().makeRequest(jsonObject);
+                boolean passflag = JsonUtil.convertFromJsonPasswordResponse(response);
+
+                if (passflag == true) {
+
+                    Platform.runLater(() -> {
+
+                        passwordText.setText("");
+                        invalidLabel.setText("");
+
+                        //   go to home page
+                        //  pass id to home
+                        ((Node) eventSource.getSource()).getScene().getWindow().hide();
+                        FXMLLoader fxload = new FXMLLoader(getClass().getResource("HomeView.fxml"));
+                        Parent root;
+                        try {
+                            root = (Parent) fxload.load();
+                            Stage stage = new Stage();
+
+                            stage.setScene(new Scene(root));
+                            stage.initStyle(StageStyle.UNDECORATED);
+
+                            stage.show();
+                        } catch (IOException ex) {
+                            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+
+                    });
+
+                } else {
+                    Platform.runLater(() -> {
+                        invalidLabel.setText("Wrong password");
+                    });
+                }
+            }).start();
+
         } else {
-            invalidLabel.setText("Wrong password");
+            Platform.runLater(() -> {
+                invalidLabel.setText("Enter valid password");
+            });
         }
-        return checkPassword;
+
     }
+
 }
